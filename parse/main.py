37382a556882e90c312e84583803fa9eee8f99f4
4YJ <ohman0705@naver.com>
2022-08-06 18:26:52 +0900

    Get metadata

--- parse/main.py
+++ parse/main.py
@@ -1,13 +1,8 @@
-import requests
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-url = 'https://brunch.co.kr/@trendlite/220'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get(url, headers=headers)
-soup = BeautifulSoup(data.text, 'html.parser')
+url = urlopen('Mock URL')
+soup = BeautifulSoup(url.read(), 'html.parser')
+get = soup.find("p", "this-is-title")
 
-title = soup.select_one('meta[property="og:title"]')['content']
-image = soup.select_one('meta[property="og:image"]')['content']
-desc = soup.select_one('meta[property="og:description"]')['content']
-print(title, image, desc)
+print(get)
