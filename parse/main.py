a3ec5ede9b7f71dce282d55fed324c10d329f542
4YJ <ohman0705@naver.com>
2022-08-07 23:28:44 +0900

    changed name

--- parse/main.py
+++ parse/main.py
@@ -4,7 +4,7 @@ from posts import import_posts
 
 print('DONE ✅')
 
-with open('view/src/mockups/post_list.json', 'w') as product_file:
-    json.dump(import_posts(), product_file,
+with open('view/src/mockups/post_list.json', 'w') as fp:
+    json.dump(import_posts(), fp,
               ensure_ascii=False, default=str, indent=2)
-    product_file.write('\n')
+    fp.write('\n')
