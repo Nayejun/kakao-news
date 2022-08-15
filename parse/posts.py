b5bca03b80ab03de4bd9d796ee7ed8261b518009
4YJ <ohman0705@naver.com>
2022-08-15 22:02:05 +0900

    renewal

--- parse/posts.py
+++ parse/posts.py
@@ -1,41 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-ROOT_URL = 'https://helloworld.kurly.com'
+ROOT_URL = 'https://tech.kakao.com/blog/'
 
 
 def import_posts():
-    url = 'https://helloworld.kurly.com'
+    url = "https://tech.kakao.com/blog/page/1/#posts"
+
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
     }
+
     data = requests.get(url, headers=headers)
     soup = BeautifulSoup(data.text, 'html.parser')
-    find_posts = soup.find_all('li', class_='post-card')
+    find_posts = soup.find_all('div', class_='elementor-post__text')
 
     this_is_posts = []
-    for find_post in find_posts:
-        post = find_post.find(
-            'a', class_='post-link')
-        image = find_post.find(
-            'img', attrs={'class': 'image-avatar'})
-        title = find_post.find(
-            'h3', class_='post-title').get_text()
-        summary = find_post.find(
-            'p', class_='title-summary')
-        url = ROOT_URL + post.attrs['href']
 
-        if (summary == None or image == None):
-            this_is_posts.append({
-                'title': title,
-                'href': url,
-            })
-        else:
-            this_is_posts.append({
-                'title': title,
-                'summary': summary.text,
-                'href': url,
-                'image': image.attrs['src'],
-            })
+    for find_post in find_posts:
+        post = find_post.find_all(
+            'div', class_='elementor-post__text')
+        title = find_post.find(
+            'h3').find('a').get_text()
+        title_arrangement = title.replace('\t', '').replace('\n', '')
+        this_is_posts.append({
+            'title': title_arrangement,
+        })
 
     return this_is_posts
